14b315593be4d64bfb3a3e328712641b578b4994
estudart <erico.studartdf@gmail.com>
2025-06-29 01:43:06 -0300

    feat: add try catch block to binance's adapter methods

--- order-manager/src/infrastructure/adapters/binance_adapter.py
+++ order-manager/src/infrastructure/adapters/binance_adapter.py
@@ -4,6 +4,7 @@ from binance.client import Client
 from dotenv import load_dotenv
 
 from src.infrastructure.adapters import OrderAdapter
+from src.infrastructure.adapters import LoggerAdapter
 
 load_dotenv()
 
@@ -13,10 +14,13 @@ class BinanceAdapter(OrderAdapter):
     def __init__(self,
                  endpoint: str = os.environ.get(f"BINANCE_ENDPOINT_{ENV}"),
                  api_key: str = os.environ.get(f"BINANCE_API_KEY_{ENV}"), 
-                 api_secret: str = os.environ.get(f"BINANCE_API_SECRET_{ENV}")):
+                 api_secret: str = os.environ.get(f"BINANCE_API_SECRET_{ENV}"),
+                 logger = LoggerAdapter().get_logger()):
+        
         self.endpoint = endpoint
         self.api_key = api_key
         self.api_secret = api_secret
+        self.logger = logger
 
         self.client = None
 
@@ -26,10 +30,19 @@ class BinanceAdapter(OrderAdapter):
         self.client = Client(self.api_key, self.api_secret)
         self.client.API_URL = self.endpoint
 
-    def send_order(self, order_data: dict) -> None:
-        order = self.client.create_order(**order_data)
-        print(order)
+    def send_order(self, order_data: dict) -> dict:
+        try:
+            order = self.client.create_order(**order_data)
+            self.logger.info(f"Order was sent to Binance: {order}")
+            return order
+        except Exception as err:
+            self.logger.error(f"Could not send order to Binance, reason: {err}")        
+            raise
 
-    def get_open_orders(self):
-        open_orders = self.client.get_open_orders()
-        return open_orders
+    def get_open_orders(self) -> list[dict]:
+        try:
+            open_orders = self.client.get_open_orders()
+            return open_orders
+        except Exception as err:
+            self.logger.error(f"Could not retrive open orders from Binance, reason: {err}")
+            raise 
