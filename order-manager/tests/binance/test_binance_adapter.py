45fbaed840c7389d16a7e18bdf118f5df142df15
estudart <erico.studartdf@gmail.com>
2025-07-03 17:22:30 -0300

    fix: refactor binance test

--- order-manager/tests/binance/test_binance_adapter.py
+++ order-manager/tests/binance/test_binance_adapter.py
@@ -4,7 +4,7 @@ class TestBinanceAdapter:
     def setup_method(self):
         self.binanace_adapter = BinanceAdapter()
 
-    def test_send_order(self):
+    def test_create_manage_order(self):
         order_data = {
             "symbol": "BTCUSDT",
             "side": "BUY",
@@ -13,16 +13,17 @@ class TestBinanceAdapter:
             "quantity": 0.001,
             "price": "30000"
         }
-        self.binanace_adapter.send_order(order_data)
+        order = self.binanace_adapter.send_order(order_data)
+        order_id = order["orderId"]
 
-    def test_get_order(self):
-        order = self.binanace_adapter.get_order("BTCUSDT", "492537")
+        order = self.binanace_adapter.get_order("BTCUSDT", order_id)
+        print(order)
         assert isinstance(order, dict)
+
+        delete = self.binanace_adapter.cancel_order("BTCUSDT", order_id)
+        assert delete
     
     def test_get_open_orders(self):
         open_orders = self.binanace_adapter.get_open_orders()
+        print(open_orders)
         assert isinstance(open_orders, list)
-
-    def test_cancel_order(self):
-        delete = self.binanace_adapter.cancel_order("BTCUSDT", "492537")
-        assert delete
