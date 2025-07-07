33b44c467de15da2a3ef08dc76c5dd16354a5235
estudart <erico.studartdf@gmail.com>
2025-07-07 11:28:30 -0300

    feat: add get order adapter logic from dict

--- order-manager/src/domain/orders/order_creation_manager.py
+++ order-manager/src/domain/orders/order_creation_manager.py
@@ -1,21 +1,30 @@
 import logging
 
-from src.infrastructure.adapters import OrderAdapter, LoggerAdapter
+from src.infrastructure.adapters import OrderAdapter, BinanceAdapter, LoggerAdapter
 from src.application.orders import Order
 
 
 
 class OrderCreationManager:
-    def __init__(self, 
-                 order_adapter: OrderAdapter,
+    def __init__(self,
                  logger: logging.Logger = LoggerAdapter.get_logger()):
-        self.order_adapter = order_adapter
         self.logger = logger
+        self.order_adapter_dict = {
+            "binance": BinanceAdapter()
+        }
+
+    def get_order_adapter(self, exchange_name: str) -> OrderAdapter:
+        try:
+            return self.order_adapter_dict[exchange_name]
+        except KeyError as err:
+            self.logger.error("Exchange requested is not valid")
+            raise
     
-    def create_order(self, order_data: dict):
+    def create_order(self, exchange_name: str, order_data: dict):
         try:
             order = Order(**order_data)
-            self.order_adapter.send_order(order.to_dict())
+            order_adapter = self.get_order_adapter(exchange_name)
+            order_adapter.send_order(order.to_dict())
         except Exception as err:
             self.logger.error(f"Could not create order, reason: {err}")
             raise OrderCreationError(f"Unable to create order, reason: {err}") from err
