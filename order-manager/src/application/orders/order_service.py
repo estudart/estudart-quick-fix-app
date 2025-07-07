e744f755ef504725c163922a0f36dd06612c48e5
estudart <erico.studartdf@gmail.com>
2025-07-07 12:26:23 -0300

    fix: refactor order service to be inside application

---
+++ order-manager/src/application/orders/order_service.py
@@ -0,0 +1,36 @@
+import logging
+
+from src.infrastructure.adapters import OrderAdapter, BinanceAdapter, LoggerAdapter
+from src.domain.orders import OrderCreationManager
+
+
+
+class OrderService:
+    def __init__(self,
+                 logger: logging.Logger = LoggerAdapter().get_logger()):
+        self.logger = logger
+        self.order_creation_manager = OrderCreationManager(logger=self.logger)
+        self.order_adapter_dict = {
+            "binance": BinanceAdapter(logger=self.logger)
+        }
+
+    def get_order_adapter(self, exchange_name: str) -> OrderAdapter:
+        try:
+            return self.order_adapter_dict[exchange_name]
+        except KeyError as err:
+            self.logger.error("Exchange requested is not valid")
+            raise ValueError("Unsupported exchange")
+    
+    def create_order(self, exchange_name: str, order_data: dict):
+        try:
+            order = self.order_creation_manager.create_order(order_data)
+            order_adapter = self.get_order_adapter(exchange_name)
+            response = order_adapter.send_order(order.to_dict())
+            return response
+        except Exception as err:
+            self.logger.error(f"Could not create order, reason: {err}")
+            raise OrderCreationError(f"Unable to create order, reason: {err}") from err
+
+
+class OrderCreationError(Exception):
+    pass
--- order-manager/src/domain/orders/order_creation_manager.py
+++ order-manager/src/domain/orders/order_creation_manager.py
@@ -1,33 +1,18 @@
-import logging
-
-from src.infrastructure.adapters import OrderAdapter, BinanceAdapter, LoggerAdapter
-from src.application.orders import Order
+from src.infrastructure.adapters import LoggerAdapter
+from src.domain.orders import Order
 
 
 
 class OrderCreationManager:
-    def __init__(self,
-                 logger: logging.Logger = LoggerAdapter.get_logger()):
+    def __init__(self, logger = LoggerAdapter().get_logger()):
         self.logger = logger
-        self.order_adapter_dict = {
-            "binance": BinanceAdapter()
-        }
 
-    def get_order_adapter(self, exchange_name: str) -> OrderAdapter:
-        try:
-            return self.order_adapter_dict[exchange_name]
-        except KeyError as err:
-            self.logger.error("Exchange requested is not valid")
-            raise
-    
-    def create_order(self, exchange_name: str, order_data: dict):
+    def create_order(self, order_data: dict):
         try:
             order = Order(**order_data)
-            order_adapter = self.get_order_adapter(exchange_name)
-            order_adapter.send_order(order.to_dict())
+            return order
         except Exception as err:
-            self.logger.error(f"Could not create order, reason: {err}")
-            raise OrderCreationError(f"Unable to create order, reason: {err}") from err
+            raise OrderCreationError(f"Could not create order, reason: {err}")
 
 
 class OrderCreationError(Exception):
