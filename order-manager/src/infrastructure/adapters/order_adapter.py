248f4863a5491b89b9dddce75234b23c8b79cb77
erico.studartdf@gmail.com <erico.studartdf@gmail.com>
2025-07-14 14:11:22 -0300

    feat: add get and cancel methods to orderadapter abstract class

--- order-manager/src/infrastructure/adapters/order_adapter.py
+++ order-manager/src/infrastructure/adapters/order_adapter.py
@@ -6,3 +6,11 @@ class OrderAdapter(ABC):
     @abstractmethod
     def send_order(self):
         pass
+
+    @abstractmethod
+    def get_order(self):
+        pass
+
+    @abstractmethod
+    def cancel_order(self):
+        pass
