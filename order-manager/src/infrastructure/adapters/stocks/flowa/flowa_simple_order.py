1b095ba31b75cc8347a76878166b4d274ed19e46
erico.studartdf@gmail.com <erico.studartdf@gmail.com>
2025-07-14 14:11:47 -0300

    feat: add tranform get order mathod to flowa adapter

--- order-manager/src/infrastructure/adapters/stocks/flowa/flowa_simple_order.py
+++ order-manager/src/infrastructure/adapters/stocks/flowa/flowa_simple_order.py
@@ -1,10 +1,11 @@
 import json
 
 from src.infrastructure.adapters.stocks.flowa.flowa_adapter import FlowaAdapter
+from src.infrastructure.adapters.logger_adapter import LoggerAdapter
 
 
 class FlowaSimpleOrderAdapter(FlowaAdapter):
-    def __init__(self, logger=...):
+    def __init__(self, logger = LoggerAdapter().get_logger()):
         super().__init__(logger)
         self.suffix = "simple-order"
 
@@ -18,4 +19,17 @@ class FlowaSimpleOrderAdapter(FlowaAdapter):
             "TimeInForce": order_data["time_in_force"],
             "Quantity": order_data["quantity"],
             "Price": str(order_data["price"])
+        }
+
+    def transform_get_order(self, order_data: dict) -> dict:
+        return {
+            "order_id": order_data["StrategyId"],
+            "symbol": order_data["Symbol"],
+            "side": order_data["Side"],
+            "quantity": order_data["Quantity"],
+            "price": order_data["Price"],
+            "order_type": order_data["OrderType"],
+            "exec_qty": order_data["ExecutedQuantity"],
+            "time_in_force": order_data["TimeInForce"],
+            "status": order_data["Status"]
         }
