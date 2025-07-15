e6f50d422915a03a3dfbfde00ec21f361f0e509a
erico.studartdf@gmail.com <erico.studartdf@gmail.com>
2025-07-15 16:49:02 -0300

    fix: binance name typo

--- order-manager/tests/binance/test_binance_adapter.py
+++ order-manager/tests/binance/test_binance_adapter.py
@@ -6,7 +6,7 @@ from src.infrastructure import BinanceSimpleOrderAdapter
 
 class TestBinanceAdapter:
     def setup_method(self):
-        self.binanace_adapter = BinanceSimpleOrderAdapter()
+        self.binance_adapter = BinanceSimpleOrderAdapter()
 
     def test_create_manage_order(self):
         order_data = {
@@ -17,24 +17,24 @@ class TestBinanceAdapter:
             "order_type": "LIMIT",
             "time_in_force": "GTC",
         }
-        order_id = self.binanace_adapter.send_order(order_data)
+        order_id = self.binance_adapter.send_order(order_data)
 
-        order = self.binanace_adapter.get_order(order_id, symbol=order_data["symbol"])
+        order = self.binance_adapter.get_order(order_id, symbol=order_data["symbol"])
         assert isinstance(order, dict)
 
-        delete = self.binanace_adapter.cancel_order(order_id, symbol=order_data["symbol"])
+        delete = self.binance_adapter.cancel_order(order_id, symbol=order_data["symbol"])
         assert delete
     
     def test_get_open_orders(self):
-        open_orders = self.binanace_adapter.get_open_orders()
+        open_orders = self.binance_adapter.get_open_orders()
         assert isinstance(open_orders, list)
 
     def test_get_order_without_symbol_raises_exception(self):
         order_id = "testing"
         with pytest.raises(ValueError, match="Missing required argument: 'symbol'"):
-            self.binanace_adapter.get_order(order_id)
+            self.binance_adapter.get_order(order_id)
     
     def test_cancel_order_without_symbol_raises_exception(self):
         order_id = "testing"
         with pytest.raises(ValueError, match="Missing required argument: 'symbol'"):
-            self.binanace_adapter.cancel_order(order_id)
+            self.binance_adapter.cancel_order(order_id)
