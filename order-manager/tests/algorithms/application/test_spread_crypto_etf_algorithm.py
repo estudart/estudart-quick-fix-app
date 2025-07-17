3ea903a9e3b866de332ff1f3d51d772482fb3d1a
erico.studartdf@gmail.com <erico.studartdf@gmail.com>
2025-07-17 18:17:20 -0300

    feat: add spread tests

--- order-manager/tests/algorithms/application/test_spread_crypto_etf_algorithm.py
+++ order-manager/tests/algorithms/application/test_spread_crypto_etf_algorithm.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 
 from src.application.algorithms.spread_crypto_etf import SpreadCryptoETFAdapter
 from src.application.orders.order_service import OrderService
@@ -9,11 +10,11 @@ from src.infrastructure.adapters import LoggerAdapter, HashdexMDAdapter, Binance
 def make_algo(overrides: dict = None) -> SpreadCryptoETF:
     base_data = {
         "broker": "935",
-        "account": "1001",
+        "account": "84855",
         "symbol": "BITH11",
         "side": "BUY",
-        "quantity": 1,
-        "spread_threshold": 0.03
+        "quantity": 100,
+        "spread_threshold": 0.02
     }
     if overrides:
         base_data.update(overrides)
@@ -36,6 +37,36 @@ class TestSpreadCryptoETFAdapter:
         print(stocks_order_params)
 
     def test_can_send_stock_order(self):
-        assert self.application_algo.send_stock_order(
-            "flowa", "simple-order", 30
+        assert self.application_algo.send_stock_order("flowa", "simple-order", 30)
+
+    def test_can_update_stock_order(self):
+        order_id = self.application_algo.send_stock_order("flowa", "simple-order", 30)
+        update = self.application_algo.update_stock_order(order_id, "flowa", "simple-order", price=55)
+        assert update
+    
+    def test_get_order_placement_price(self):
+        stock_fair_price = 150
+        spread_threshold = 0.1
+
+        order_placement_price_buy = self.application_algo.get_order_placement_price(
+            stock_fair_price=stock_fair_price,
+            side="BUY",
+            spread_threshold=spread_threshold
         )
+
+        order_placement_price_sell = self.application_algo.get_order_placement_price(
+            stock_fair_price=stock_fair_price,
+            side="SELL",
+            spread_threshold=spread_threshold
+        )
+
+        assert order_placement_price_buy == 135
+        assert order_placement_price_sell == 165
+    
+    def test_can_run_cycle(self):
+        order_id = self.application_algo.send_stock_order("flowa", "simple-order", 30)
+        cumulative_qty = 0
+        while cumulative_qty != self.application_algo.algo.algo_data["quantity"]:
+            cumulative_qty += self.application_algo.run_cycle(order_id, cumulative_qty)
+            time.sleep(5)
+        assert cumulative_qty
