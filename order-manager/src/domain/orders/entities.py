6d24128acd7a736e51ea8cde9eb1a095bcc0cdb0
erico.studartdf@gmail.com <erico.studartdf@gmail.com>
2025-07-17 13:06:59 -0300

    feat: add market order logic to entities

--- order-manager/src/domain/orders/entities.py
+++ order-manager/src/domain/orders/entities.py
@@ -6,7 +6,6 @@ class SimpleOrder:
     def __init__(
             self, 
             quantity: float, 
-            price: float, 
             symbol: str,
             side: str,
             order_type: str,
@@ -15,7 +14,7 @@ class SimpleOrder:
         ):
     
         self.quantity = quantity
-        self.price = price
+        self.price = kwargs.get("price", None)
         self.symbol = symbol
         self.side = side
         self.order_type = order_type
@@ -27,29 +26,35 @@ class SimpleOrder:
     def _validate(self) -> bool:
         if not (
             isinstance(self.quantity, (int, float)) and
-            isinstance(self.price, (int, float)) and
             isinstance(self.symbol, str)
         ):
             raise TypeError("Types are incorrect")
         
         if self.quantity <= 0:
             raise ValueError("Quantity should be greater then 0")
-        if self.price <= 0:
-            raise ValueError("Price should be greater then 0")
         if not self.symbol:
             raise ValueError("Symbol is required")
+        
+        if self.order_type == "LIMIT":
+            if self.price <= 0:
+                raise ValueError("Price should be greater then 0")
+            if not isinstance(self.price, (int, float)):
+                raise TypeError("Types are incorrect")
         
         return True
     
     def to_dict(self) -> dict:
         order_data = {
             "quantity": self.quantity,
-            "price": self.price,
             "symbol": self.symbol,
             "side": self.side,
             "order_type": self.order_type,
             "time_in_force": self.time_in_force
         }
+        if self.order_type == "LIMIT":
+            order_data.update(
+                price=self.price
+            )
         # Logic for sending orders to Flowa
         if self.broker:
             order_data.update(
