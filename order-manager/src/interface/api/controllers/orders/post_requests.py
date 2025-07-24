5ff2283b8e9a8d24ad287d16ff66983d5bf332cc
erico.studartdf@gmail.com <erico.studadf@gmail.com>
2025-07-24 13:04:04 -0300

    feat: add try catch block for send order request

--- order-manager/src/interface/api/controllers/orders/post_requests.py
+++ order-manager/src/interface/api/controllers/orders/post_requests.py
@@ -1,14 +1,21 @@
 from dependency_injector.wiring import inject, Provide
 
 from src.application.orders.order_service import OrderService
+from src.infrastructure.adapters.order_adapter import SendOrderError
 from src.interface.api.containers import Container
 
 
 
 @inject
 def send_order_request(data: dict, order_service: OrderService = Provide[Container.order_service]):
-    return order_service.send_order(
-        exchange_name=data["exchange_name"],
-        strategy=data["strategy"],
-        order_data=data["order_data"]
-    )
+    try:
+        return order_service.send_order(
+            exchange_name=data["exchange_name"],
+            strategy=data["strategy"],
+            order_data=data["order_data"]
+        )
+    except SendOrderError as err:
+        return {
+            "success": False,
+            "message": f"{err}"
+        }
