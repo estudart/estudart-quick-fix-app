344b9e71f9ab468fdfb00f365d5811f68437d55c
erico.studartdf@gmail.com <erico.studadf@gmail.com>
2025-07-26 14:19:35 -0300

    fix: algo manager and algofactory split in application and domain

--- order-manager/src/domain/algorithms/algo_factory.py
+++ order-manager/src/domain/algorithms/algo_factory.py
@@ -3,7 +3,7 @@ from src.domain.algorithms.entities import SpreadCryptoETF
 
 
 
-class AlgoManager:
+class AlgoFactory:
     def __init__(self, logger = LoggerAdapter().get_logger()):
         self.logger = logger
         self.algo_dict = {
@@ -14,27 +14,13 @@ class AlgoManager:
     def create_algo(self, algo_name: str, algo_data: dict):
         try:
             algo_class = self.algo_dict[algo_name]
-            algo = algo_class(**algo_data)
+            algo = algo_class(algo_data)
             self.active_algos[algo.id] = algo
             return algo
         except Exception as err:
             raise AlgoCreationError(
                 f"Could not create algorithm, reason: {err}"
             )
-
-    def stop_algo(self, algo_id: str):
-        algo = self.active_algos.get(algo_id)
-
-        if not algo:
-            raise ValueError(f"Algo with id: {algo_id}, was not found")
-        
-        algo.stop()
-        self.logger.info(f"Algo with id: {algo_id} was stopped")
-        del self.active_algos[algo_id]
-
-    def update_algo(self):
-        pass
-
 
 class AlgoCreationError(Exception):
     pass
