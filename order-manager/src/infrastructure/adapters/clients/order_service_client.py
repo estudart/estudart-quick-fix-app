cbc4c5ba6c13d057b4ae97f84b0aca16abe1354f
erico.studartdf@gmail.com <erico,studartdf@gmail.com>
2025-07-29 18:24:02 -0300

    feat: add cancel order to order service

--- order-manager/src/infrastructure/adapters/clients/order_service_client.py
+++ order-manager/src/infrastructure/adapters/clients/order_service_client.py
@@ -20,9 +20,13 @@ class OrderServiceClient:
             response = self.client.post(f"{self.base_url}/send-order", params=params)
             response.raise_for_status()
             return response.json()
-        except httpx.HTTPError as err:
+        except (httpx.HTTPError, httpx.HTTPStatusError) as err:
             self.logger.error(f"[OrderHttpClient] Failed to send order: {err}")
             raise
+        except Exception as err:
+            self.logger.exception(f"[OrderHttpClient] Failed to send order with unkown exception: {err}")
+            raise
+        
 
     def get_order(self, exchange_name: str, strategy: str, order_id: dict, **kwargs) -> dict:
         try:
@@ -35,8 +39,11 @@ class OrderServiceClient:
             response = self.client.get(f"{self.base_url}/get-order", params=params)
             response.raise_for_status()
             return response.json()
-        except httpx.HTTPError as err:
-            self.logger.error(f"[OrderHttpClient] Failed to send order: {err}")
+        except (httpx.HTTPError, httpx.HTTPStatusError) as err:
+            self.logger.error(f"[OrderHttpClient] Failed to get order: {err}")
+            raise
+        except Exception as err:
+            self.logger.exception(f"[OrderHttpClient] Failed to get order with unkown exception: {err}")
             raise
 
     def update_order(self, exchange_name: str, strategy: str, order_id: dict, order_data: dict, **kwargs) -> dict:
@@ -51,6 +58,27 @@ class OrderServiceClient:
             response = self.client.put(f"{self.base_url}/update-order", params=params)
             response.raise_for_status()
             return response.json()
-        except httpx.HTTPError as err:
-            self.logger.error(f"[OrderHttpClient] Failed to send order: {err}")
+        except (httpx.HTTPError, httpx.HTTPStatusError) as err:
+            self.logger.error(f"[OrderHttpClient] Failed to update order: {err}")
+            raise
+        except Exception as err:
+            self.logger.exception(f"[OrderHttpClient] Failed to update order with unkown exception: {err}")
+            raise
+
+    def cancel_order(self, exchange_name: str, strategy: str, order_id: str, **kwargs) -> dict:
+        try:
+            params = {
+                "exchange_name": exchange_name,
+                "strategy": strategy,
+                "order_id": order_id,
+                **kwargs
+            }
+            response = self.client.delete(f"{self.base_url}/cancel-order", params=params)
+            response.raise_for_status()
+            return response.json()
+        except (httpx.HTTPError, httpx.HTTPStatusError) as err:
+            self.logger.error(f"[OrderHttpClient] Failed to cancel order: {err}")
+            raise
+        except Exception as err:
+            self.logger.exception(f"[OrderHttpClient] Failed to cancel order with unkown exception: {err}")
             raise
