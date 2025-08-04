6476613b9642c1c1d19699f73baa9a03c0988cc4
erico.studartdf@gmail.com <erico.studartdf@gmail.com>
2025-08-04 16:25:20 -0300

    fix: remove unused methods from redis

--- order-manager/src/infrastructure/adapters/queue/redis_adapter.py
+++ order-manager/src/infrastructure/adapters/queue/redis_adapter.py
@@ -56,21 +56,6 @@ class RedisAdapter:
                 self.logger.info(f"Key: {query}, Value: {data}")
             except Exception as err:
                 self.logger.error(f"Could not add data to query: {err}")
-    
-
-    def append_to_set(self, query, data, send_time):
-        if self.redis_db:
-            key_type = self.redis_db.type(query).decode('utf-8')
-            self.logger.debug(f"key_type: {key_type}")
-            try:
-                self.redis_db.zadd(query, {json.dumps(data): send_time})
-                self.logger.info(f"Time to expire set: {self.redis_db.ttl(query)}")
-                if self.redis_db.ttl(query) == -1:
-                    self.redis_db.expire(query, int(60*60*24*2)) # expires in two days
-                self.logger.info(f"Appended data to Redis set '{query}': {json.dumps(data)}, send_time: {send_time}")
-            except Exception as err:
-                self.logger.info(f"Could not append data to redis, reason: {err}")
-
 
     def get_key(self, key):
         """
@@ -89,33 +74,6 @@ class RedisAdapter:
         except Exception as err:
             self.logger.error(f"Error getting key from Redis: {err}")
             return None
-
-    
-    def get_queue(self, queue):
-        message_data = self.redis_db.rpop(queue)
-        self.logger.debug(f"Consumed data from queue: {queue}, data: {message_data}")
-        if not message_data:
-            self.logger.debug(f"Queue '{queue}' is empty or no messages fetched.")
-            return {"status": False, "message": {}}
-        
-        message_json = json.loads(message_data)
-        return {"status": True, "message": message_json}
-
-
-    def get_batch_queue(self, queue, count=100):
-        messages_data = self.redis_db.rpop(queue, count)  # Attempt to pop up to `count` messages
-
-        self.logger.debug(f"Consumed {len(messages_data) if messages_data else 0} messages from queue: {queue}")
-
-        if not messages_data:
-            self.logger.debug(f"Queue '{queue}' is empty or no messages fetched.")
-            return {"status": False, "messages": []}
-
-        # Convert JSON strings to Python dictionaries
-        messages_json = [json.loads(msg) for msg in messages_data]
-        
-        return {"status": True, "messages": messages_json}
-    
     
     def insert_to_queue(self, message_data, queue):
         message_json = json.dumps(message_data, default=str)
